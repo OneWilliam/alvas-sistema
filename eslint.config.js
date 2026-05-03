236ef44fee48a10f9d330ab5d356c7e1de01eee5
OneWilliam <wyvargasc@unjbg.edu.pe>
2026-05-02 21:34:31 -0500

    feat: agregando eslint

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+const js = require("@eslint/js");
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+
+module.exports = [
+  {
+    ignores: [
+      "eslint.config.js",
+      "node_modules/**",
+      "dist/**",
+      "out/**",
+      "coverage/**",
+      ".wrangler/**",
+      ".cache/**",
+      "bun.lock",
+    ],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        Bun: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "no-undef": "off",
+    },
+  },
+];
